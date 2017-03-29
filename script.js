8aa3a1ef4bfe54af804563d4b35fadebafe10430
carlosAdelphic <carlos@Carlos-Lopez-Plandolit.local>
2017-03-29 12:31:50 +0100

    corrected issue with script.js

--- script.js
+++ script.js
@@ -184,9 +184,9 @@ function addMacros (adserver, tag)
 			document.getElementById('tagwithmacros').value = finaltag;
 			break;
 
-	}
+	
 
-	case ("DCM_Legacy"):
+		case ("DCM_Legacy"):
 
 			var cbm = tag.indexOf("\;ord");
 			var cm = tag.indexOf("\;click");
@@ -223,9 +223,9 @@ function addMacros (adserver, tag)
 			document.getElementById('tagwithmacros').value = finaltag;
 			break;
 
-	}
+	
 
-	case ("DCM_INS"):
+		case ("DCM_INS"):
 
 			//var cbm = tag.indexOf("\;ord"); INS tags don't require our cache buster macros
 			var cm = tag.indexOf("data-dcm-click-tracker");
